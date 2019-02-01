7dcd92ebb852682cf2a072ba928a6fdb6ae2d066
Miguel Ferreira <miguel.ferreira.2003@outlook.com>
2019-02-01 23:48:58 +0000

    ColGaia-Connect Alpha v1.0

---
+++ brooke.py
@@ -0,0 +1,4 @@
+# ColGaia Connect
+This is an open source chat app, in the terminal, for me to talk with my friends in school (don't do that kids ahaha)
+
+Be free to use it and make pull requests with changes! Can't wait to see your awesome changes!!!
